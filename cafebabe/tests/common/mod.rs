dfb9c56c0566e70595f0c3a908cf2ec3647f31b0
Maarten Mulders <m.th.mulders@xs4all.nl>
2025-03-31 09:09:37 +0200

    test: Improve logging setup for tests

--- cafebabe/tests/common/mod.rs
+++ cafebabe/tests/common/mod.rs
@@ -1,11 +1,16 @@
+use std::sync::Once;
 use simplelog::{ColorChoice, CombinedLogger, Config, LevelFilter, TermLogger, TerminalMode};
 
+static LOGGING_STATE: Once = Once::new();
+
 pub fn setup_logging() {
-    let logger = TermLogger::new(
-        LevelFilter::Debug,
-        Config::default(),
-        TerminalMode::Mixed,
-        ColorChoice::Auto,
-    );
-    CombinedLogger::init(vec![logger]).unwrap();
+    LOGGING_STATE.call_once(|| {
+        let logger = TermLogger::new(
+            LevelFilter::Debug,
+            Config::default(),
+            TerminalMode::Mixed,
+            ColorChoice::Auto,
+        );
+        CombinedLogger::init(vec![logger]).unwrap();
+    });
 }
